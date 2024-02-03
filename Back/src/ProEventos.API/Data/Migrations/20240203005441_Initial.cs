abe72fbc0e3328afbde137356014fb33a9391796
wylkerd <wylkerd@hotmail.com>
2024-02-02 22:22:13 -0300

    Gerada Migration e Banco de dados

---
+++ Back/src/ProEventos.API/Data/Migrations/20240203005441_Initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProEventos.API.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    EventoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Local = table.Column<string>(type: "TEXT", nullable: false),
+                    DataEvento = table.Column<string>(type: "TEXT", nullable: false),
+                    Tema = table.Column<string>(type: "TEXT", nullable: false),
+                    QtdPessoas = table.Column<int>(type: "INTEGER", nullable: false),
+                    Lote = table.Column<string>(type: "TEXT", nullable: false),
+                    ImagemURL = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.EventoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eventos");
+        }
+    }
+}
